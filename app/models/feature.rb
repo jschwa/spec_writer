a3b18f72b686f178ce45d109f952699ee9c42ffc
pwoloszun <maciej.zubala@gmail.com>
2013-05-22 17:12:32 +0200

    progress

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,6 +1,16 @@
 class Feature < ActiveRecord::Base
   attr_accessible :back_end, :front_end, :title
 
-  validates_presence_of :back_end, :front_end, :title
+  validate :at_least_one_filed_defined
+
+  private
+
+  def at_least_one_filed_defined
+    if title.blank? && front_end.blank? && back_end.blank?
+      [:title, :front_end, :back_end].each do |field|
+        errors.add(field, "at least one of title, front end and back end should be definded")
+      end
+    end
+  end
 
 end
