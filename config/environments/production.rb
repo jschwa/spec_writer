80a84d61e3beaaf53acff22c43e393683cf031ca
Maciej Zubala <maciej.zubala@gmail.com>
2013-09-17 16:00:36 +0200

    Production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,4 +71,6 @@ SpecWriter::Application.configure do
   # Precompile additional assets
   config.assets.precompile += %w( .svg .eot .woff .ttf )
 
+  config.assets.initialize_on_precompile = false
+
 end
