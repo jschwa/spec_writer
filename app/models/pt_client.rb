b7743ebe1a065594ab50acffc70d1e0a37e50e4d
Maciej Zubala <maciej.zubala@gmail.com>
2014-01-14 17:32:30 +0100

    Sync with pivotal tracker (the other way)

--- app/models/pt_client.rb
+++ app/models/pt_client.rb
@@ -2,6 +2,8 @@ class PTClient
 
   PROJECTS_URL = "/projects"
   STORIES_URL = "/projects/{project_id}/stories"
+  FRONTEND_MARKER = "=== FRONTEND [don't change this line] ==="
+  BACKEND_MARKER = "=== BACKEND [don't change this line] ==="
 
   def initialize api_key
     @api_key = api_key
@@ -44,7 +46,8 @@ class PTClient
   end
 
   def create_story page, item, description_type = nil
-    if item_doesnt_exist_in_pt?(item, description_type)
+    existing_story = existing_pt_item(item, description_type)
+    if existing_story.nil?
       feature = item.itemizable
       story_params = {
         project_id: page.pt_info.project_id,
@@ -54,19 +57,37 @@ class PTClient
       response = pt_request(STORIES_URL.gsub("{project_id}", story_params[:project_id].to_s), :post, story_params)
       item.add_pt_item_info(response, description_type)
     else
-      update_story(page, item, description_type)
+      update_story(page, item, existing_story, description_type)
     end
-
   end
 
-  def update_story page, item, description_type = nil?
-    feature = item.itemizable
-    story_params = {
-      story_type: "feature",
-    }
-    story_params = story_params.merge(page.pt_info.separate_front_end_from_back_end? ? separate_story_params(page, feature, description_type) : single_story_params(page, feature))
-    response = pt_request(STORIES_URL.gsub("{project_id}", page.pt_info.project_id.to_s)+"/#{item.pt_item_for(description_type).story_id}", :put, story_params)
-    item.update_pt_item_info(response)
+  def update_story page, item, existing_story, description_type = nil?
+    if Time.parse(existing_story["updated_at"]) < item.itemizable.updated_at
+      feature = item.itemizable
+      story_params = {
+        story_type: "feature",
+      }
+      story_params = story_params.merge(page.pt_info.separate_front_end_from_back_end? ? separate_story_params(page, feature, description_type) : single_story_params(page, feature))
+      response = pt_request(STORIES_URL.gsub("{project_id}", page.pt_info.project_id.to_s)+"/#{item.pt_item_for(description_type).story_id}", :put, story_params)
+      item.update_pt_item_info(response)
+    else
+      update_story_from_pt(page, item, existing_story, description_type)
+    end
+  end
+
+  def update_story_from_pt page, item, existing_story, description_type
+    attributes_to_update = {title: existing_story["name"]}
+    description = existing_story["description"]
+    if description_type.nil?
+      parts = description.split(BACKEND_MARKER)
+      frontend = parts.first.gsub(FRONTEND_MARKER, "")
+      backend = parts[1]
+      attributes_to_update = attributes_to_update.merge(front_end: frontend, back_end: backend)
+    else
+      attributes_to_update[description_type] = description
+    end
+    item.itemizable.update_attributes(attributes_to_update)
+    item.update_pt_item_info(existing_story)
   end
 
   def separate_story_params page, feature, description_type
@@ -85,13 +106,13 @@ class PTClient
         name: page.name
       }],
       description: "
-       FRONTEND
- 
-       #{feature.front_end}
- 
-       BACKEND
- 
-       #{feature.back_end}
+      #{FRONTEND_MARKER}
+
+      #{feature.front_end}
+
+      #{BACKEND_MARKER}
+
+      #{feature.back_end}
       "
     }
   end
@@ -101,14 +122,14 @@ class PTClient
     "https://www.pivotaltracker.com/services/v5#{url}"
   end
 
-  def item_doesnt_exist_in_pt?(item, description_type)
+  def existing_pt_item(item, description_type)
     response = pt_request(STORIES_URL.gsub("{project_id}", item.page.pt_info.project_id.to_s))
     stories_json = JSON.parse(response)
     found_story_json = stories_json.find do |story_json|
       pt_item_info = item.pt_item_for(description_type)
       pt_item_info.same_story_id?(story_json) unless pt_item_info.nil?
     end
-    found_story_json.nil?
+    found_story_json
   end
 
 end
